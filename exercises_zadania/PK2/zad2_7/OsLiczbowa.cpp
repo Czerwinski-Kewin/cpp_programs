7e8c8e1ab5eb9a8450404cec7b6c81d5b2702671
Czerwinski-Kewin <73647814+Czerwinski-Kewin@users.noreply.github.com>
2024-05-05 16:51:15 +0200

    0505

---
+++ exercises_zadania/PK2/zad2_7/OsLiczbowa.cpp
@@ -0,0 +1,85 @@
+#include "OsLiczbowa.h"
+#include <iostream>
+using namespace std;
+
+void OsLiczbowa::set(int _min, int _max, int _interval)
+{
+	min = _min;
+	max = _max;
+	interval = _interval;
+}
+
+int OsLiczbowa::getMin()
+{
+	return min;
+}
+
+int OsLiczbowa::getMax()
+{
+	return max;
+}
+
+int OsLiczbowa::getInterval()
+{
+	return interval;
+}
+
+void OsLiczbowa::write()
+{
+	cout << getMin() << " " << getMax() << " " << getInterval() << " " << endl;
+}
+
+int OsLiczbowa::licznik{ 0 };
+int OsLiczbowa::wypLicznik()
+{
+	cout << "Licznik: " << licznik << endl;
+	return licznik;
+}
+
+OsLiczbowa::OsLiczbowa() {
+	min = 5;
+	max = 10;
+	interval = 1;
+	cout << "OsLiczbowa - konstruktor domyslny" << endl;
+	licznik++;
+}
+
+OsLiczbowa::OsLiczbowa(int _min, int _max) {
+	//this->min = min;	- lepiej stosowac podkreslenia
+	min = _min;
+	max = _max;
+	interval = (max - min) / max;
+	cout << "OsLiczbowa - konstruktor dwuargumentowy" << endl;
+}
+
+OsLiczbowa::OsLiczbowa(const OsLiczbowa& amin, const OsLiczbowa& amax, const OsLiczbowa& ainterval) {
+	min = amin.min;
+	max = amax.max;
+	interval = ainterval.interval;
+	cout << "OsLiczbowa - konstruktor wieloargumentowy kopiujacy" << endl;
+}
+
+
+OsLiczbowa::~OsLiczbowa() {
+	cout << "OsLiczbowa - destruktor" << endl;
+	licznik--;
+	//wypLicznik();
+};
+
+
+ostream& operator <<(ostream& ostr, const OsLiczbowa& obj)
+{
+	ostr << obj.min << " " << obj.max << " " << obj.interval << endl;
+	return ostr;
+}
+
+
+OsLiczbowa& OsLiczbowa::operator=(const OsLiczbowa& r)
+{
+	cout << "OsLiczbowa - operator przypisania" << endl;
+	min = r.min;
+	max = r.max;
+	interval = r.interval;
+
+	return *this;
+}
---
+++ exercises_zadania/PK2/zad2_7/ZbiorOsi.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "OsLiczbowa.h"
+class ZbiorOsi
+{
+	int n;
+public:
+	OsLiczbowa* tab = nullptr;
+	ZbiorOsi();
+	ZbiorOsi(int _n);
+	ZbiorOsi(const ZbiorOsi& cpn);
+	ZbiorOsi(ZbiorOsi&& kp);
+	~ZbiorOsi();
+	friend ostream& operator << (ostream& ostr, const ZbiorOsi& obj);
+	ZbiorOsi& operator=(const ZbiorOsi& r);
+	ZbiorOsi& operator=(ZbiorOsi&& r);
+};
+
