528d65accff2a723bba3f91fcb9b660b828bfe21
Czerwinski-Kewin <73647814+Czerwinski-Kewin@users.noreply.github.com>
2024-05-05 17:00:45 +0200

    0505

---
+++ exercises_zadania/PK2/lab2/zad4/Źródło.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "Nag³ówek.h"
+#include "OsLiczbowa.h"
+#include "ZbiorOsi.h"
+using namespace std;
+
+
+void auxFun(OsLiczbowa osarg)
+{
+	OsLiczbowa os3(osarg, osarg, osarg);
+	OsLiczbowa* ptr = new OsLiczbowa(osarg);
+	delete ptr;
+}
+
+int main()
+{
+	OsLiczbowa os;
+	OsLiczbowa os2;
+	//OsLiczbowa::wypLicznik();
+	OsLiczbowa os3;
+	//OsLiczbowa os2(5, 10);
+	//OsLiczbowa os3(os, os, os2);
+	//auxFun(os2);
+	//os.set(4, 28, 2);
+	//os.write();
+	//cout << os;		//friend ostream zamiast write
+
+	ZbiorOsi zbior = ZbiorOsi(3);
+	cout << zbior;
+
+
+	return 0;
+}
