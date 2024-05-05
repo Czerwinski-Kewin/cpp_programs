76b642a72f195a268598a3335fc9ca30efdcb643
Czerwinski-Kewin <73647814+Czerwinski-Kewin@users.noreply.github.com>
2024-05-05 17:22:24 +0200

    0505

---
+++ exercises_zadania/PK2/lab3/zad3_6/Wykres.cpp
@@ -0,0 +1,39 @@
+#include "Wykres.h"
+
+Wykres::Wykres(int _rozmiar, string _nazwa, Typ _typ)
+{
+	nazwa = _nazwa;
+	typ = _typ;
+	cout << "Wykres - konstruktor wieloargumentowy" << endl;
+}
+
+Wykres::Wykres() : Wykres(5, "wyk1", Typ::Sunburst)
+{
+	cout << "Wykres - konstruktor bezargumentowy" << endl;;
+}
+
+Wykres::~Wykres()
+{
+	cout << "Wykres - destruktor" << endl;
+}
+
+Wykres::Wykres(const Wykres& cpn) : Wykres(5, cpn.nazwa, cpn.typ) {
+	//nazwa = cpn.nazwa;
+	//typ = cpn.typ;
+	cout << "Wykres - konstuktor kopiujacy" << endl;
+}
+
+Wykres::Wykres(Wykres&& kp) {
+	nazwa = kp.nazwa;
+	typ = kp.typ;
+	cout << "Wykres - konstruktor przenoszacy" << endl;
+}
+
+ostream& operator <<(ostream& ostr, static Wykres& obj)
+{
+	ostr << obj.nazwa << endl;
+	ostr << static_cast<ZbiorOsi&>(obj);
+	return ostr;
+}
+
+
