30dd87937600e1a2d2929e3947441f6c974cb0fb
Czerwinski-Kewin <73647814+Czerwinski-Kewin@users.noreply.github.com>
2024-05-05 17:49:45 +0200

    0505

---
+++ exercises_zadania/PK2/lab4/zad4_1/Źródło.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include "Nag³ówek.h"
+#include "OsLiczbowa.h"
+#include "ZbiorOsi.h"
+#include "Wykres.h"
+using namespace std;
+
+
+void auxFun(OsLiczbowa osarg)
+{
+	OsLiczbowa os3(osarg, osarg, osarg);
+	OsLiczbowa* ptr = new OsLiczbowa(osarg);
+	delete ptr;
+}
+
+void przyklad()
+{
+	//Wykres wyk;
+	//cout << wyk;
+	Wykres wyk2(7, "wyk2", Typ::Sunburst);
+	Wykres wyk;
+	//wyk = wyk2;
+	wyk2 = move(wyk);
+	cout << wyk;
+	ZbiorOsi zbior = ZbiorOsi(3);
+	//zbior++;
+}
+
+int main()
+{
+	//OsLiczbowa os;
+	//OsLiczbowa os2;
+	//OsLiczbowa::wypLicznik();
+	//OsLiczbowa os3;
+	//OsLiczbowa os2(5, 10);
+	//OsLiczbowa os3(os, os, os2);
+	//auxFun(os2);
+	//os.set(4, 28, 2);
+	//os.write();
+	//cout << os;		//friend ostream zamiast write
+
+	/*
+	cout << "Lewe argumenty przed: " << endl;
+	ZbiorOsi zbior = ZbiorOsi(3);
+	ZbiorOsi zbior2 = ZbiorOsi(3);
+
+	zbior = move(zbior2);
+
+	//cout << zbior;
+	cout << "Lewe argumenty po: " << endl;
+	*/
+
+	//OsLiczbowa test;
+	//test.set(1, 3, 100);
+	//test *= 2;
+	//cout << test;
+	//ZbiorOsi zbior3 = ZbiorOsi(3) + test;
+	//przyklad();
+	Wykres wyk;
+	wyk.draw();
+
+
+
+	return 0;
+}
