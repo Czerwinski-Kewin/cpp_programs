1aae9511caf1bc9ad1db326594f4d52b7722393a
Czerwinski-Kewin <73647814+Czerwinski-Kewin@users.noreply.github.com>
2024-05-05 16:42:16 +0200

    0505

---
+++ exercises_zadania/PK2/lab1/zad5/Źródło.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "Nag³ówek.h"
+#include "OsLiczbowa.h"
+using namespace std;
+
+
+void auxFun(OsLiczbowa osarg)
+{
+	OsLiczbowa os3(osarg, osarg, osarg);
+	OsLiczbowa* ptr = new OsLiczbowa(osarg);
+	delete ptr;
+}
+
+int main()
+{
+	OsLiczbowa os;
+	OsLiczbowa os2;
+	OsLiczbowa::wypLicznik();
+	OsLiczbowa os3;
+	//OsLiczbowa os2(5, 10);
+	//OsLiczbowa os3(os, os, os2);
+	//auxFun(os2);
+	os.set(4, 28, 2);
+	os.write();
+	//os.wypLicznik();
+	OsLiczbowa::wypLicznik();
+
+	return 0;
+}
+
